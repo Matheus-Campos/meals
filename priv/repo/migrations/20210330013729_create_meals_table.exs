f05286328d0ac7c6b4a4a19d3fa1ce43628d9c18
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-29 22:44:23 -0300

    feat: create meals table

---
+++ priv/repo/migrations/20210330013729_create_meals_table.exs
@@ -0,0 +1,11 @@
+defmodule Meals.Repo.Migrations.CreateMealsTable do
+  use Ecto.Migration
+
+  def change do
+    create table :meals do
+      add :descricao, :string
+      add :data, :utc_datetime
+      add :calorias, :integer
+    end
+  end
+end
