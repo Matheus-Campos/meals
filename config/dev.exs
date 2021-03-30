230cfe76a28f445df5be4633f9753399e0d7ceb8
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-29 22:35:54 -0300

    chore: configure development database credentials

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :meals, Meals.Repo,
   username: "postgres",
   password: "postgres",
-  database: "meals_dev",
+  database: "meals",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
