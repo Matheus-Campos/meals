88ee3ec72d840fc5aeb2a9ce815543ebf9ec2ca0
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-30 07:47:39 -0300

    feat: add meal schema

---
+++ lib/meals/meal.ex
@@ -0,0 +1,25 @@
+defmodule Meals.Meal do
+  use Ecto.Schema
+
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:descricao, :data, :calorias]
+  @permitted_params @required_params ++ [:id]
+
+  schema "meals" do
+    field :descricao, :string
+    field :data, :utc_datetime
+    field :calorias, :integer
+
+    timestamps()
+  end
+
+  def changeset(struct \\ %__MODULE__{}, params) do
+    struct
+    |> cast(params, @permitted_params)
+    |> validate_required(@required_params)
+    |> validate_number(:calorias, greater_than: 0)
+  end
+end
