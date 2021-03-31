29dbfc743e79ab80fc24ee3ba070f5c59876a1ad
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-30 23:15:58 -0300

    test: add tests for get meal module

---
+++ test/meals/meal/get_test.exs
@@ -0,0 +1,30 @@
+defmodule Meals.Meal.GetTest do
+  use Meals.DataCase
+
+  import Meals.Factory
+
+  alias Meals.Meal
+  alias Meals.Meal.Get
+
+  describe "by_id/1" do
+    test "when the meal is found, returns it" do
+      %Meal{id: id} = meal = insert(:meal)
+
+      response = Get.by_id(id)
+
+      expected_response = {:ok, meal}
+
+      assert response == expected_response
+    end
+
+    test "when the meal is not found, returns an error" do
+      %Meal{id: id} = build(:meal)
+
+      response = Get.by_id(id)
+
+      expected_response = {:error, %Meals.Error{message: "Meal not found", status: :not_found}}
+
+      assert response == expected_response
+    end
+  end
+end
--- test/support/factory.ex
+++ test/support/factory.ex
@@ -1,11 +1,24 @@
 defmodule Meals.Factory do
-  use ExMachina
+  use ExMachina.Ecto, repo: Meals.Repo
+
+  alias Meals.Meal
 
   def meal_params_factory do
     %{
       descricao: "Risole de frango",
-      data: DateTime.utc_now(),
+      data: DateTime.to_string(DateTime.utc_now()),
       calorias: 70
+    }
+  end
+
+  def meal_factory do
+    %Meal{
+      id: "fef2d8c3-e41e-4315-a8a1-f54c7f915474",
+      descricao: "Risole de frango",
+      data: DateTime.utc_now(),
+      calorias: 70,
+      inserted_at: DateTime.utc_now(),
+      updated_at: DateTime.utc_now()
     }
   end
 end
