c35d87a57d7a050870ad134feae625a4965f1a1a
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-30 23:16:07 -0300

    test: fix create meal tests

--- test/meals/meal/create_test.exs
+++ test/meals/meal/create_test.exs
@@ -3,14 +3,20 @@ defmodule Meals.Meal.CreateTest do
 
   import Meals.Factory
 
+  alias Ecto.Changeset
   alias Meals.{Error, Meal}
   alias Meals.Meal.Create
+
+  defp transform_map_keys_to_string(map) do
+    Enum.into(map, %{}, fn {key, value} -> {Atom.to_string(key), value} end)
+  end
 
   describe "call/1" do
     test "when all params are valid, returns a meal" do
       response =
         :meal_params
         |> build()
+        |> transform_map_keys_to_string()
         |> Create.call()
 
       assert {:ok, %Meal{calorias: 70, data: _data, descricao: "Risole de frango"}} = response
@@ -20,9 +26,14 @@ defmodule Meals.Meal.CreateTest do
       response =
         :meal_params
         |> build(calorias: 0)
+        |> transform_map_keys_to_string()
         |> Create.call()
 
-      assert {:error, %Error{message: "User not found", status: :not_found}} = response
+      assert {:error, %Changeset{changes: %{calorias: 0}, valid?: false} = changeset} = response
+
+      expected_errors = %{calorias: ["must be greater than 0"]}
+
+      assert errors_on(changeset) == expected_errors
     end
   end
 end
