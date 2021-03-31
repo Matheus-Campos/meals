14f36a93dcbde25e55f30e1570c3e97b960d50c0
Matheus Campos da Silva <matheus.campos@mesainc.com.br>
2021-03-30 23:33:47 -0300

    feat: add delete meal module

--- lib/meals.ex
+++ lib/meals.ex
@@ -1,7 +1,9 @@
 defmodule Meals do
   alias Meals.Meal.Create
+  alias Meals.Meal.Delete
   alias Meals.Meal.Get
 
   defdelegate create_meal(params), to: Create, as: :call
   defdelegate get_by_id(id), to: Get, as: :by_id
+  defdelegate delete_by_id(id), to: Delete, as: :call
 end
