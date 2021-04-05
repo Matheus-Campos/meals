f5234719a5d7f5ffb49bcb4fde773af8be691cd1
Matheus Campos da Silva <matheus.campos@compasso.com.br>
2021-04-05 16:43:39 -0300

    refactor: change aliases names

--- lib/meals.ex
+++ lib/meals.ex
@@ -1,17 +1,17 @@
 defmodule Meals do
-  alias Meals.Meals.Create
-  alias Meals.Meals.Delete
-  alias Meals.Meals.Get
-  alias Meals.Meals.List
-  alias Meals.Meals.Update
+  alias Meals.Meals.Create, as: CreateMeal
+  alias Meals.Meals.Delete, as: DeleteMeal
+  alias Meals.Meals.Get, as: GetMeal
+  alias Meals.Meals.List, as: ListMeals
+  alias Meals.Meals.Update, as: UpdateMeal
 
   alias Meals.Users.Create, as: CreateUser
 
-  defdelegate create_meal(params), to: Create, as: :call
-  defdelegate get_by_id(id), to: Get, as: :by_id
-  defdelegate delete_by_id(id), to: Delete, as: :call
-  defdelegate update_meal(params), to: Update, as: :call
-  defdelegate list_meals(), to: List, as: :call
+  defdelegate create_meal(params), to: CreateMeal, as: :call
+  defdelegate get_by_id(id), to: GetMeal, as: :by_id
+  defdelegate delete_by_id(id), to: DeleteMeal, as: :call
+  defdelegate update_meal(params), to: UpdateMeal, as: :call
+  defdelegate list_meals(), to: ListMeals, as: :call
 
   defdelegate create_user(params), to: CreateUser, as: :call
 end
