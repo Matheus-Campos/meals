057b0e545e5b981545e0e3cf8cfacac4d59a1481
Matheus Campos da Silva <matheus.campos@compasso.com.br>
2021-04-05 17:24:42 -0300

    feat: returns user in meals list

--- lib/meals/meals/list.ex
+++ lib/meals/meals/list.ex
@@ -1,5 +1,12 @@
 defmodule Meals.Meals.List do
+  import Ecto.Query
+
   alias Meals.{Meal, Repo}
 
-  def call, do: Repo.all(Meal)
+  def call do
+    query = from meal in Meal, preload: [:user]
+
+    query
+    |> Repo.all()
+  end
 end
